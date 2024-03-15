0fc8e6e146040eb9130413f46956cb1ef4d3a850
Zerohertz <ohg3417@gmail.com>
2024-03-15 17:52:23 +0900

    :hammer: Add: pyright

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,6 +2,11 @@ return {
   "nvim-lspconfig",
   opts = {
     servers = {
+      pyright = {
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function() end,
+        },
+      },
       ruff_lsp = {
         on_attach = function(client, _)
           client.server_capabilities.hoverProvider = false
