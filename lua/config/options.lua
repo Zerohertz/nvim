4f1bae21be03ee14043dd3331816aba85c9107fc
Zerohertz <ohg3417@gmail.com>
2024-03-15 12:49:35 +0900

    :art: Update: Window Bar

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 local opt = vim.opt
 
 -- Indentation
@@ -24,6 +20,7 @@ opt.termguicolors = true
 opt.signcolumn = "yes"
 
 -- Etc
+opt.winbar = "%=%m %f"
 opt.encoding = "UTF-8"
 opt.cmdheight = 1
 opt.scrolloff = 10
