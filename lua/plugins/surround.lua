2bf2b1b70255f5d9005b1ff70a3724adc2f1e997
Zerohertz <ohg3417@gmail.com>
2024-04-08 13:51:48 +0900

    :bento: Add: Surround

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,18 @@
+return {
+  "kylechui/nvim-surround",
+  config = function()
+    require("nvim-surround").setup({
+      keymaps = {
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+        change_line = "cS",
+      },
+    })
+  end,
+}
