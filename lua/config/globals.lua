0a33514715fa17c0edb878a1d1612370334d4529
Zerohertz <ohg3417@gmail.com>
2024-12-27 11:05:54 +0900

    :bug: fix: Waiting for OSC 52 response from the terminal. Press Ctrl-C to interrupt… ('https://github.com/neovim/neovim/discussions/28010#discussioncomment-9877494')

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -17,7 +17,7 @@ end
 vim.g.mapleader = " " -- global leader
 vim.g.maplocalleader = " " -- local leader
 
-if is_ssh() or is_container() then
+if is_ssh() then
   vim.g.clipboard = {
     name = "OSC 52",
     copy = {
@@ -27,6 +27,25 @@ if is_ssh() or is_container() then
     paste = {
       ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
       ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+elseif is_container() then
+  vim.o.clipboard = "unnamedplus"
+  local function paste()
+    return {
+      vim.fn.split(vim.fn.getreg(""), "\n"),
+      vim.fn.getregtype(""),
+    }
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
     },
   }
 end
