a28ce74a8f2cf88996f8ea42d34c656609686305
Zerohertz <ohg3417@gmail.com>
2025-02-13 00:23:50 +0900

    :hammer: fix: fzf

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,15 @@
 return {
   "nvim-lspconfig",
-  opts = {
-    servers = {
+  opts = function(_, opts)
+    local Keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- stylua: ignore
+    vim.list_extend(Keys, {
+      { "gd", "<cmd>FzfLua lsp_definitions     jump1=true ignore_current_line=true<cr>", desc = "Goto Definition", has = "definition" },
+      { "gr", "<cmd>FzfLua lsp_references      jump1=true ignore_current_line=true<cr>", desc = "References", nowait = true },
+      { "gI", "<cmd>FzfLua lsp_implementations jump1=true ignore_current_line=true<cr>", desc = "Goto Implementation" },
+      { "gy", "<cmd>FzfLua lsp_typedefs        jump1=true ignore_current_line=true<cr>", desc = "Goto T[y]pe Definition" },
+    })
+    opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
       pyright = {
         handlers = {
           ["textDocument/publishDiagnostics"] = function() end,
@@ -15,6 +23,7 @@ return {
       clangd = {
         filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
       },
-    },
-  },
+    })
+    return opts
+  end,
 }
